1bf344426f596dd8ed7f8cf16aa3737bc2ace25a
fberci <fberci@20e696c2-e7d1-468f-abf3-6b607df430dd>
2007-03-26 19:27:54 +0000

    Lassan, de haladok. :)

--- trunk/phpBB3/language/hu/mcp.php
+++ trunk/phpBB3/language/hu/mcp.php
@@ -4,7 +4,7 @@
 * mcp [Hungarian]
 *
 * @package language
-* @version $Id: mcp.php,v 1.4 2007-03-24 22:39:35 fberci Exp $
+* @version $Id: mcp.php,v 1.5 2007-03-26 19:27:54 fberci Exp $
 * @copyright (c) 2005 phpBB Group 
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License 
 *
@@ -35,7 +35,7 @@ $lang = array_merge($lang, array(
 	'ADD_FEEDBACK'			=> 'Feljegyzés hozzáadása', //? fejlegyzés? hozzáadás?
 	'ADD_FEEDBACK_EXPLAIN'	=> 'Ha hozzá szeretnél adni egy feljegyzést, töltsd ki az alábbi űrlapot. Csak sima szöveget írj, a HTML, BBCode stb. nem engedélyezett.', //?? "If you would like to add a report on this please fill out the following form. Only use plain text; HTML, BBCode, etc. are not permitted."
 	'ADD_WARNING'			=> 'Felhasználó figyelmeztetése', //? 'Figyelmeztetés', 'Figyelmeztetés hozzáadása'
-	'ADD_WARNING_EXPLAIN'	=> 'Ha figyelmezetetni szeretnéd ezt a felhasználót, töltsd ki az alábbi űrlpoat. Csak sima szöveget írj, a HTML, BBCode stb. nem engedélyezett.',
+	'ADD_WARNING_EXPLAIN'	=> 'Ha figyelmezetetni szeretnéd ezt a felhasználót, töltsd ki az alábbi űrlapot. Csak sima szöveget írj, a HTML, BBCode stb. nem engedélyezett.',
 	'ALL_ENTRIES'			=> 'Összes bejegyzés', //?
 	'ALL_NOTES_DELETED'		=> 'Az összes felhasználói megjegyzés sikeresen törlésre került.', //? "Successfully removed all user notes"
 	'ALL_REPORTS'			=> 'Összes jelentés',
@@ -158,31 +158,31 @@ $lang = array_merge($lang, array(
 	'MCP_MAKE_NORMALS'				=> 'Normál témává tétel',
 	'MCP_MAKE_NORMALS_CONFIRM'		=> 'Biztosan normál témává akarod tenni a kiválsztott témákat?',
 
-	'MCP_NOTES'						=> 'User notes',
-	'MCP_NOTES_FRONT'				=> 'Front page',
-	'MCP_NOTES_USER'				=> 'User details',
+	'MCP_NOTES'						=> 'Felhasználói feljegyzések', //?
+	'MCP_NOTES_FRONT'				=> 'Kezdőlap',
+	'MCP_NOTES_USER'				=> 'Felhasználói adatok', //?
 
-	'MCP_REPORTS'					=> 'Reported posts',
-	'MCP_REPORT_DETAILS'			=> 'Report details',
-	'MCP_REPORTS_CLOSED'			=> 'Closed reports',
-	'MCP_REPORTS_CLOSED_EXPLAIN'	=> 'This is a list of all reports about posts which have previously been resolved',
-	'MCP_REPORTS_OPEN'				=> 'Open reports',
-	'MCP_REPORTS_OPEN_EXPLAIN'		=> 'This is a list of all reported posts which are still to be handled',
+	'MCP_REPORTS'					=> 'Jelentett hozzászólások',
+	'MCP_REPORT_DETAILS'			=> 'Részletes jelentés', //?
+	'MCP_REPORTS_CLOSED'			=> 'Lezárt jelentések',
+	'MCP_REPORTS_CLOSED_EXPLAIN'	=> 'Ez a lista azokat a jelentéseket tartalmazza, melyek már megoldásra kerültek.', //? megoldás?
+	'MCP_REPORTS_OPEN'				=> 'Nyitott jelentések',
+	'MCP_REPORTS_OPEN_EXPLAIN'		=> 'Ez a lista azokat a jelentéseket tartalmazza, melyek még megoldásra várnak.', //?
 
-	'MCP_QUEUE'								=> 'Moderation queue',
-	'MCP_QUEUE_APPROVE_DETAILS'				=> 'Approve details',
-	'MCP_QUEUE_UNAPPROVED_POSTS'			=> 'Posts awaiting approval',
-	'MCP_QUEUE_UNAPPROVED_POSTS_EXPLAIN'	=> 'This is a list of all posts which require approving before they will be visible to users',
-	'MCP_QUEUE_UNAPPROVED_TOPICS'			=> 'Topics awaiting approval',
-	'MCP_QUEUE_UNAPPROVED_TOPICS_EXPLAIN'	=> 'This is a list of all topics which require approving before they will be visible to users',
+	'MCP_QUEUE'								=> 'Moderálandók', //?????????? 'Moderációs sor'??? 'Moderálásra váró elemek'??
+	'MCP_QUEUE_APPROVE_DETAILS'				=> 'Hozzászólás adatok', //?? "Approve details"
+	'MCP_QUEUE_UNAPPROVED_POSTS'			=> 'Jóváhagyásra váró hozzászólások',
+	'MCP_QUEUE_UNAPPROVED_POSTS_EXPLAIN'	=> 'Ez a lista azokat a hozzászólásokat tartalmazza, melyek egyelőre nem láthatók a felhasználók számára, mivel még nem kerültek jóváhagyásra.', //?
+	'MCP_QUEUE_UNAPPROVED_TOPICS'			=> 'Jóváhagyásra váró témák',
+	'MCP_QUEUE_UNAPPROVED_TOPICS_EXPLAIN'	=> 'Ez a lista azokat a témákat tartalmazza, melyek egyelőre nem láthatók a felhasználók számára, mivel még nem kerültek jóváhagyásra.',
 
-	'MCP_VIEW_USER'			=> 'View warnings for a specific user',
+	'MCP_VIEW_USER'			=> 'Meghatározott felhasználó figyelmeztetéseinek megtekintése', //? "View warnings for a specific user" - használják egyáltalán?
 
-	'MCP_WARN'				=> 'Warnings',
-	'MCP_WARN_FRONT'		=> 'Front page',
-	'MCP_WARN_LIST'			=> 'List warnings',
-	'MCP_WARN_POST'			=> 'Warn for specific post',
-	'MCP_WARN_USER'			=> 'Warn user',
+	'MCP_WARN'				=> 'Figyelmeztetések',
+	'MCP_WARN_FRONT'		=> 'Kezdőlap',
+	'MCP_WARN_LIST'			=> 'Figyelmeztetések listája',
+	'MCP_WARN_POST'			=> 'Figyelmeztetés egy bizonyos hozzászólásért', //? "Warn for specific post" meghatározott?
+	'MCP_WARN_USER'			=> 'Felhasználó figyelmeztetése',
 
 	'MERGE_POSTS'			=> 'Merge posts',
 	'MERGE_POSTS_CONFIRM'	=> 'Are you sure you want to merge the selected posts?',
