b68bc3552a839ef9be3a5a8a91c787f224354df7
nobody <nobody@20e696c2-e7d1-468f-abf3-6b607df430dd>
2007-05-20 20:47:52 +0000

    This commit was manufactured by cvs2svn to create tag 'release- UI_B1-phpBB_3_0_RC1'.

---
+++ tags/release-UI_B1-phpBB_3_0_RC1/phpBB3/language/hu/search_synonyms.php
@@ -0,0 +1,15 @@
+<?php
+/** 
+*
+* search_synonyms [English]
+*
+* @package language
+* @version $Id: search_synonyms.php,v 1.3 2007-03-17 11:33:37 fberci Exp $
+* @copyright (c) 2005 phpBB Group 
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+$synonyms = array(
+);
+?>
