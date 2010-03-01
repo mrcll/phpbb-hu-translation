65b1744d5930f95f88d23cdaea5570dc65d79b11
fberci <fberci@20e696c2-e7d1-468f-abf3-6b607df430dd>
2010-03-01 23:28:54 +0000

    3.0.7 címke

---
+++ tags/release-phpBB_3_0_7/phpBB3/language/hu-x-formal/search_synonyms.php
@@ -0,0 +1,17 @@
+<?php
+/** 
+*
+* search_synonyms [English]
+*
+* @package language
+* @version $Id$
+* @copyright (c) 2007 „Magyar phpBB Közösség fordítók”
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+* Original copyright: (c) 2005 phpBB Group
+
+*
+*/
+
+$synonyms = array(
+);
+?>
