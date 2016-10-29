ec96dae42387a474c7a5ee20d4913e4ff8591297
Máté Bartus <mate.bartus@gmail.com>
2016-10-29 18:08:32 +0200

    Kisebb problémák javítása

--- language/hu/help/bbcode.php
+++ language/hu/help/bbcode.php
@@ -33,7 +33,7 @@ if (!defined('IN_PHPBB'))
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
-$help = array(
+$lang = array(
 	array(
 		0 => '--',
 		1 => 'Bevezető'
